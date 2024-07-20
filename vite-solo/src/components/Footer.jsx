04812b33cf9b0f49a6a902a2d4ae1327ddbd6353
ntwalii <ntwaliaubin@gmail.com>
2024-07-20 20:03:14 +0200

    Pre-finished digital business card

---
+++ vite-solo/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Footer(){
+    return (
+    <div className="footer">
+        <img src="./assets/Twitter.png" alt="" />
+        <img src="./assets/Facebook.png" alt="" />
+        <img src="./assets/Instagram.png" alt="" />
+        <img src="./assets/Linkedin.png" alt="" />
+        <img src="./assets/GitHub.png" alt="" />
+    </div>
+    )
+}
+
---
+++ vite-solo/src/components/Info.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Info(){
+    return (
+        <div className="info">
+        <img src="./assets/profile.jpg" className="profile" alt="" />
+        <h2>Aubin Ntwali</h2>
+        <p>Frontend Developer</p>
+        <p>Ntwali.tech</p>
+        <button> 
+            Email
+        </button>
+        </div>
+    )
+}
